9521d1e41804f3982e1f5774d62f70744d518120
preritasaini1 <149650718+preritasaini1@users.noreply.github.com>
2023-11-02 13:17:45 +0530

    func2.py

--- func2.py
+++ func2.py
@@ -1,4 +1,13 @@
-def start_adventure():
+'''def start_adventure():
     hero_name=input("enter your hero's name")
     print(f"welcome,{hero_name}! you embark on an epic adventure.")
-start_adventure()
+start_adventure()'''
+
+
+high_score=0  #global score
+def update_high_score(score):
+    global high_score
+    if score>high_score:
+        high_score=score
+update_high_score(50)
+print(high_score)
